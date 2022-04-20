c40c98312cf40c701d3468ec9f66d632111183f7
angelgiovanna <aescobar2045@gmail.com>
2022-04-20 15:28:10 -0400

    ;)

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'Deadrat55-',
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
